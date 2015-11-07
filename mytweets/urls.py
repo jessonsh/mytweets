3d66436db1232a78417c11c7c4aefe66f76aca13
jesson <jessonsh@126.com>
2015-11-07 15:10:03 +0800

    at sub branch

--- mytweets/urls.py
+++ mytweets/urls.py
@@ -21,5 +21,5 @@ from tweets import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='test'),
+    url(),
 ]
